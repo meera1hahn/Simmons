86a27f3b9d0c7071135a890f082e57512e6f46fa
Meera Hahn <meerahahn@gatech.edu>
2018-10-31 13:45:45 -0400

    too out comments

--- bag2mp4.m
+++ bag2mp4.m
@@ -1,33 +1,33 @@
 clc;    % Clear the command window.
 clear;
 directory = '/home/meerahahn/Documents/Projects/Simmons/';
-video = 'left_45_3ft';
+video = 'right_45_3ft';
 bag = rosbag(strcat(directory,'Bag_Files/',video,'.bag')); % Load the rosbag.
 
-% %Get color stream
-% display('extracting color stream')
-% img_dir = strcat(directory,'Color_Streams/',video,'/');
-% bSel = select(bag, 'Topic', '/device_0/sensor_1/Color_0/image/data');
-% msgs = readMessages(bSel);
-% for i = 1 : numel(msgs)
-%     img = readImage(msgs{i});
-%     name = sprintf('%08d',i);
-%     imwrite(img,strcat(img_dir,name,'.png'));
-% end
-% 
-% %Save color stream as avi video
-% display('saving color stream as video')
-% imageNames = dir(fullfile(img_dir,'*.png'));
-% imageNames = {imageNames.name}';
-% videoname = strcat(img_dir, '.avi');
-% outputVideo = VideoWriter(videoname);
-% outputVideo.FrameRate = 30;
-% open(outputVideo)
-% for ii = 1:length(imageNames)
-%    img = imread(fullfile(img_dir,imageNames{ii}));
-%    writeVideo(outputVideo,img)
-% end
-% close(outputVideo)
+%Get color stream
+display('extracting color stream')
+img_dir = strcat(directory,'Color_Streams/',video,'/');
+bSel = select(bag, 'Topic', '/device_0/sensor_1/Color_0/image/data');
+msgs = readMessages(bSel);
+for i = 1 : numel(msgs)
+    img = readImage(msgs{i});
+    name = sprintf('%08d',i);
+    imwrite(img,strcat(img_dir,name,'.png'));
+end
+
+%Save color stream as avi video
+display('saving color stream as video')
+imageNames = dir(fullfile(img_dir,'*.png'));
+imageNames = {imageNames.name}';
+videoname = strcat(img_dir, '.avi');
+outputVideo = VideoWriter(videoname);
+outputVideo.FrameRate = 30;
+open(outputVideo)
+for ii = 1:length(imageNames)
+   img = imread(fullfile(img_dir,imageNames{ii}));
+   writeVideo(outputVideo,img)
+end
+close(outputVideo)
 
 %Get depth stream
 display('extracting depth stream')
